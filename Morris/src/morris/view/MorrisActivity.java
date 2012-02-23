192cc2e1bc71bc9865802366b8f858e513c30c16
olejri <olejri@gmail.com>
2012-02-23 11:47:15 +0100

    fixing Morris project
    Signed-off-by: olejri <olejri@gmail.com>

--- Morris/src/morris/view/MorrisActivity.java
+++ Morris/src/morris/view/MorrisActivity.java
@@ -9,8 +9,5 @@ public class MorrisActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        // HELLO COMMIT WORLD YOFACE
-        
     }
 }
