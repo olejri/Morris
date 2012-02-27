a7c505814eb66ebe6320d3f463938d6a414309ca
olejri <olejri@gmail.com>
2012-02-27 15:45:50 +0100

    added Android Morris project
    Signed-off-by: olejri <olejri@gmail.com>

---
+++ Morris/src/morris/game/MorrisActivity.java
@@ -0,0 +1,13 @@
+package morris.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MorrisActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
