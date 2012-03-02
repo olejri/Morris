81c1f83bf2bd394165a86747e70d507cd29c6ee5
olejri <olejri@gmail.com>
2012-03-02 12:48:56 +0100

    Added a Rect and a helplog class
    Signed-off-by: olejri <olejri@gmail.com>

--- Morris/src/morris/gui/BoardView.java
+++ Morris/src/morris/gui/BoardView.java
@@ -3,10 +3,13 @@ package morris.gui;
 
 import java.util.ArrayList;
 
+import morris.help.LogHelp;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
@@ -19,6 +22,16 @@ import android.view.View;
 
 
 public class BoardView extends View {
+	
+	int xLeft = 0;
+	int yTop = 0;
+	int xRight = 470;
+	int yBottom = 470;
+	int viewWidth = 0;
+    int viewHeight = 0;
+	LogHelp l = new LogHelp();
+	
+	
 
 	public BoardView(Context context) {
 		super(context);	
@@ -32,8 +45,14 @@ public class BoardView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		//For scaling
+		drawBoard(canvas);
+		
 
+	}
+	protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+        super.onSizeChanged(xNew, yNew, xOld, yOld);
+        viewWidth = xNew;
+        viewHeight = yNew;
 	}
 
 	@Override
@@ -41,7 +60,34 @@ public class BoardView extends View {
 		//This method is overridden in both local and network-class
 		return true;
 	}
-
+	
+	
+	public void drawBoard(Canvas canvas){
+		Paint p = getPaint(Color.BLACK);
+		Rect r = new Rect(xLeft, yTop, viewWidth-1, viewWidth);
+		l.Out("Test"+ p.getStrokeWidth());
+		Log.i("LOGHELP", "Width" + viewWidth + "Height" + viewHeight);
+		Log.i("LOGHELP", ""+p.getStrokeWidth());
+		canvas.drawRect(r, p);
+		
+		
+		
+		
+		
+	}
+	
+	public Rect makeRect(){
+		Rect r = new Rect();
+		return r;
+	}
+	
+	public Paint getPaint(int color){
+		Paint p = new Paint();
+		p.setStyle(Style.STROKE);
+		p.setColor(color);
+		return p;
+		
+	}
 
 	
 
