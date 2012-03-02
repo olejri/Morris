f3e712f6f76a48fd784291024b7d49a7c8412d67
Stian Sorebo <stian.soerebo@gmail.com>
2012-03-02 15:37:10 +0100

    Laget ny constant class og ryddet i MorrisActivity
    Signed-off-by: Stian Sorebo <stian.soerebo@gmail.com>

--- Morris/src/morris/game/MorrisActivity.java
+++ Morris/src/morris/game/MorrisActivity.java
@@ -1,5 +1,7 @@
 package morris.game;
 
+import morris.help.Constant;
+
 import com.skiller.api.listeners.SKBaseListener;
 import com.skiller.api.listeners.SKOnTurnbasedGameChosenListener;
 import com.skiller.api.operations.SKApplication;
@@ -11,80 +13,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.Button;
 
 public class MorrisActivity extends Activity {
-	
-	
-	public MorrisActivity ma = this;
-	
+
+
 	SKApplication skMorris;
-	
-	
+	private int screen_width; 
+	private int screen_height; 
+
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
-		
+
 		//GameHandler.getInstance().setMenuContext(ma);
-		
-		Display display = ma.getWindowManager().getDefaultDisplay();
-		
-		int screen_width = display.getWidth();
-		int screen_height = display.getHeight();
-		String deviceId = null;
-		String user_agent = null;
-		
-		//Skiller id's for permission to use the Skiller Tools
-		String app_key = "465abbaa5ebd41498903a2a42576f639";
-		String app_secret = "d7a8155968d9437781f82f10df7c5188";
-		String app_id = "934190831255";
-		
-		skMorris = new SKApplication(app_id, app_key, app_secret, "1", 0);
-		
-		skMorris.login(ma, screen_width, screen_height, deviceId, user_agent, new SKBaseListener(){
+
+		Display display = getWindowManager().getDefaultDisplay();
+		screen_width = display.getWidth();
+		screen_height = display.getHeight();
+
+		skMorris = new SKApplication(Constant.app_id, Constant.app_key, Constant.app_secret, "1", 0);
+
+		skMorris.login(this, screen_width, screen_height, null, null, new SKBaseListener(){
 			public void onResponse(SKBaseResponse st){
-				
+
 			}
-		});
+		});	
 		
-		
-		Button openNewGameButton = (Button) findViewById(R.id.menu_button_creategame);
-		Button joinGameButton = (Button) findViewById(R.id.menu_button_joingame);
-		Button helpGameButton = (Button) findViewById(R.id.menu_button_help);
-		
-		
-		openNewGameButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				GameHandler.getInstance().clearGame();
-				GameHandler.getInstance().createNewGame();
-				Intent intent=new Intent(MorrisActivity.this, PlayGameActivity.class);
-				startActivity(intent);
-			}
-		});
-		
-		joinGameButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				GameHandler.getInstance().clearGame();
-				skMorris.getUIManager().showTurnbasedGamesLobbyScreen(ma, new SKOnTurnbasedGameChosenListener(){
-					public void onResponse(SKTurnbasedGameChosenResponse arg0){
-						//skMorris.getGameManager().getTurnBasedTools().joinGame(arg0.getGameId(), new StartGameListener());
-					}
-				});
-			}
-		});
-		
-		helpGameButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				Intent intent=new Intent(MorrisActivity.this, HelpActivity.class);
-				startActivity(intent);	
-			}
-		});
-		
+	}
+
+	public void onClick(View view) {
+		Intent i = new Intent();
+		if (view.getId() == R.id.menu_button_creategame) {
+			GameHandler.getInstance().clearGame();
+			GameHandler.getInstance().createNewGame();
+			i.setClass(this, PlayGameActivity.class);
+			startActivity(i);
+		} else if (view.getId() == R.id.menu_button_joingame) {
+			GameHandler.getInstance().clearGame();
+			skMorris.getUIManager().showTurnbasedGamesLobbyScreen(this, new SKOnTurnbasedGameChosenListener(){
+				public void onResponse(SKTurnbasedGameChosenResponse arg0){
+					//skMorris.getGameManager().getTurnBasedTools().joinGame(arg0.getGameId(), new StartGameListener());
+				}
+			});
+		} else if (view.getId() == R.id.menu_button_help) {
+			i.setClass(this, HelpActivity.class);
+			startActivity(i);
+		}
+
 	}
 
 }
---
+++ Morris/src/morris/help/Constant.java
@@ -0,0 +1,10 @@
+package morris.help;
+
+public class Constant {
+	
+	//Skiller id's for permission to use the Skiller Tools
+	public static final String app_key = "465abbaa5ebd41498903a2a42576f639";
+	public static final String app_secret = "d7a8155968d9437781f82f10df7c5188";
+	public static final String app_id = "934190831255";
+
+}
