b7d5a631027fe7e0f8ee0eb851b83a4ea77cfdfe
olejri <olejri@gmail.com>
2012-03-02 14:43:05 +0100

    working on boardView, done with Rects and Lines
    Signed-off-by: olejri <olejri@gmail.com>

--- Morris/src/morris/gui/BoardView.java
+++ Morris/src/morris/gui/BoardView.java
@@ -22,16 +22,17 @@ import android.view.View;
 
 
 public class BoardView extends View {
-	
-	int xLeft = 0;
-	int yTop = 0;
+
+	int xLeft = 10;
+	int yTop = 10;
 	int xRight = 470;
 	int yBottom = 470;
 	int viewWidth = 0;
-    int viewHeight = 0;
+	int viewHeight = 0;
 	LogHelp l = new LogHelp();
-	
-	
+	ArrayList<Point> pointList = new ArrayList<Point>();
+
+
 
 	public BoardView(Context context) {
 		super(context);	
@@ -46,13 +47,15 @@ public class BoardView extends View {
 	@Override
 	protected void onDraw(Canvas canvas) {
 		drawBoard(canvas);
-		
+
 
 	}
 	protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
-        super.onSizeChanged(xNew, yNew, xOld, yOld);
-        viewWidth = xNew;
-        viewHeight = yNew;
+		super.onSizeChanged(xNew, yNew, xOld, yOld);
+		viewWidth = xNew;
+		viewHeight = yNew;
+		xRight = viewWidth;
+		yBottom = viewWidth;
 	}
 
 	@Override
@@ -60,36 +63,75 @@ public class BoardView extends View {
 		//This method is overridden in both local and network-class
 		return true;
 	}
-	
-	
+
+
 	public void drawBoard(Canvas canvas){
-		Paint p = getPaint(Color.BLACK);
-		Rect r = new Rect(xLeft, yTop, viewWidth-1, viewWidth);
-		l.Out("Test"+ p.getStrokeWidth());
-		Log.i("LOGHELP", "Width" + viewWidth + "Height" + viewHeight);
-		Log.i("LOGHELP", ""+p.getStrokeWidth());
+		//board calc
+		int calcValue = (viewWidth-21)/6;
+		int secondRect = calcValue;
+		int thirdRect = calcValue*2;
+		
+		Paint p = getPaint();
+		//Rect r = new Rect(xLeft, yTop, viewWidth-11, viewWidth-10);
+		xRight = xRight-11;
+		yBottom = yBottom-10;
+		
+		//first Rect
+		drawRect(xLeft, yTop, xRight, yBottom, canvas, p);
+		//second Rect
+		drawRect(xLeft+secondRect, yTop+secondRect, xRight-secondRect, yBottom-secondRect, canvas, p);
+		//third Rect
+		drawRect(xLeft+thirdRect, yTop+thirdRect, xRight-thirdRect, yBottom-thirdRect, canvas, p);
+		//first Line
+		drawLine(xLeft, yBottom/2, xLeft+thirdRect, yBottom/2, canvas, p);
+		//second Line
+		drawLine(xRight-thirdRect, yBottom/2, xRight, yBottom/2, canvas, p);
+		//third Line
+		drawLine(xRight/2, yTop, xRight/2, yTop+thirdRect, canvas, p);
+		//4th line
+		drawLine(xRight/2, yBottom-thirdRect, xRight/2, yBottom, canvas, p);
+		
+		
+		p.setStyle(Style.FILL);
+		drawCircle(10, 10 , canvas, p);
+
+
+
+
+
+
+	}
+
+	public void drawRect(int xLeft, int yTop, int xRight, int yBottom, Canvas canvas, Paint p){
+		Rect r = new Rect(xLeft, yTop, xRight, yBottom);
 		canvas.drawRect(r, p);
-		
-		
-		
-		
+	}
+
+	public void drawCircle(int x, int y, Canvas canvas, Paint p){
+		canvas.drawCircle(x, y, 10, p);
 		
 	}
 	
-	public Rect makeRect(){
-		Rect r = new Rect();
-		return r;
+	public void drawLine(int startX, int startY, int stopX, int stopY, Canvas canvas, Paint p){
+		canvas.drawLine(startX, startY, stopX, stopY, p);
 	}
 	
-	public Paint getPaint(int color){
+
+	public Paint getPaint(){
 		Paint p = new Paint();
 		p.setStyle(Style.STROKE);
-		p.setColor(color);
+		p.setColor(Color.BLACK);
+		p.setStrokeWidth(6);
 		return p;
+
+	}
+	
+	public void drawPoints(Canvas canvas , Paint p){
+		
 		
 	}
 
-	
+
 
 }
 
---
+++ Morris/src/morris/gui/Point.java
@@ -0,0 +1,16 @@
+package morris.gui;
+
+public class Point {
+	private int id;
+	private int xCoor;
+	private int yCoor;
+	
+	
+	
+	public Point(int id, int x, int y){
+		this.id = id;
+		this.xCoor = x;
+		this.yCoor = y;
+	}
+
+}
