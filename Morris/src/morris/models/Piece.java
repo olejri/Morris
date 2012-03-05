7e12452cb9e93735cd7741583980fd76bef8b72b
Steinar Haram <steinar.haram@online.no>
2012-03-05 13:30:54 +0100

    Added logic to the Piece class, and piece list to the Player class.

--- Morris/src/morris/models/Piece.java
+++ Morris/src/morris/models/Piece.java
@@ -1,10 +1,45 @@
 package morris.models;
 
 public class Piece {
-	
+
 	private boolean selectable;
+	private boolean movable;
+	private int currentRow;
+	private int currentColumn;
 	
-	public boolean isSelectable(){
+	public Piece(){
+		
+	}
+	
+	public void placeAtCoordinate(int row, int column){
+		currentRow = row;
+		currentColumn = column;
+	}
+	
+	public int getCurrentRow(){
+		return currentRow;
+	}
+	
+	public int getCurrentColumn(){
+		return currentColumn;
+	}
+
+	public boolean isSelectable() {
 		return selectable;
 	}
+
+	public void setSelectable(boolean selectable) {
+		this.selectable = selectable;
+	}
+
+	public boolean isMovable() {
+		return movable;
+	}
+
+	public void setMovable(boolean movable) {
+		this.movable = movable;
+	}
+	
+	
 }
+
--- Morris/src/morris/models/Player.java
+++ Morris/src/morris/models/Player.java
@@ -1,5 +1,13 @@
 package morris.models;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	
+	private String name;
+	private ArrayList<Piece> pieces;
+
+	public Player(String name, int numberOfPieces){
+		this.name = name;
+	}
 }
