e53f3a384e611a3a4061edac12674beb2818c672
Emil Mork <emilandreasmork@dhcp-047221.wlan.ntnu.no>
2012-04-16 12:40:21 +0200

    added new superactiity class

---
+++ Morris/src/morris/game/SuperActivity.java
@@ -0,0 +1,15 @@
+package morris.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SuperActivity extends Activity {
+	
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+}
