9e14bf50074c10649c1043ce86ef008d4ee79f23
olejri <olejri@gmail.com>
2012-04-16 12:20:20 +0200

    moved state logic from BoardView to GameController
    Signed-off-by: olejri <olejri@gmail.com>

--- Morris/src/morris/game/controller/GameController.java
+++ Morris/src/morris/game/controller/GameController.java
@@ -1,9 +1,14 @@
 package morris.game.controller;
 
+import java.util.ArrayList;
 import java.util.Timer;
 
 import morris.game.PlayGameActivity;
+import morris.gui.Point;
+import morris.help.Constant;
 import morris.models.GameMove;
+import morris.models.ModelPoint;
+import morris.models.Piece;
 import morris.models.StartGame;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,9 +16,15 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 import morris.models.Game;
+import morris.states.FlyingState;
+import morris.states.MoveState;
+import morris.states.PlacementState;
+import morris.states.RemovalState;
+import morris.states.SelectState;
 
 import com.skiller.api.items.SKUser;
 import com.skiller.api.listeners.SKOnFeeChosenListener;
@@ -26,21 +37,21 @@ import com.skiller.api.responses.SKGetFeeOptionsResponse;
 public class GameController {
 
 	private static GameController instance = null;
-	
-	
+
+
 	private Context menuContext;
 	private Context canvasContext;
-	
+
 	private ProgressDialog progressDialog;
 	private Timer timer;
-	
+
 	private boolean serverEndGameresponse=false;
 	private boolean gameStarted=false;
 	private boolean waiting_for_opponnent = false;
 	private boolean canvasContextON=false;
 	private boolean gameOwner;
 	private boolean printed = false;
-	
+
 	private static Game morrisGame = null;
 
 	private SKApplication skMorris;
@@ -48,7 +59,7 @@ public class GameController {
 	private SKUser guest;
 	private String game_id;
 	private int pot;
-	
+
 	private int turn;
 	private int side;
 
@@ -58,17 +69,17 @@ public class GameController {
 		}
 		return instance;
 	}
-	
+
 	public static Game getGame(){
 		return GameController.getInstance().getMorrisGame();
 	}
-	
+
 
 	private GameController() {
 		clearGame();
 	}
 
-	
+
 	/*
 	 *  createNewGame() method - starts a new game that other can join
 	 */
@@ -92,11 +103,11 @@ public class GameController {
 	private void chooseFeeDialog() {
 		skMorris.getUIManager().showChooseFeeScreen(menuContext,
 				new SKOnFeeChosenListener() {
-					@Override
-					public void onResponse(SKFeeChosenResponse st) {
-						startGameWithChosenFee(st.getFee());
-					}
-				});
+			@Override
+			public void onResponse(SKFeeChosenResponse st) {
+				startGameWithChosenFee(st.getFee());
+			}
+		});
 	}
 
 	/*
@@ -107,83 +118,83 @@ public class GameController {
 		String lols = skMorris.getGameManager().getTurnBasedTools().toString();
 		System.out.println("WUT" + lols);
 		skMorris.getGameManager().getTurnBasedTools()
-				.getFeeOptions(new SKOnGetFeeOptionsListener() {
+		.getFeeOptions(new SKOnGetFeeOptionsListener() {
 
-					private int minFee = 0;
-					private int maxFee = 0;
-					private String title = "Choose fee";
-					private String text;
+			private int minFee = 0;
+			private int maxFee = 0;
+			private String title = "Choose fee";
+			private String text;
 
-					@Override
-					public void onResponse(SKGetFeeOptionsResponse arg0) {
-						if (arg0.getStatusCode() == 0) {// valid response
-							minFee = arg0.getMinFee();
-							maxFee = arg0.getMaxFee();
-							text = "Game fee (" + minFee + " - " + maxFee
-									+ "): ";
-							showChooseFeeDialog(title, text);
-						} else {// error
-							text = "Game fee (" + minFee + " - " + maxFee
-									+ "): ";
-							showChooseFeeDialog(title, text);
+			@Override
+			public void onResponse(SKGetFeeOptionsResponse arg0) {
+				if (arg0.getStatusCode() == 0) {// valid response
+					minFee = arg0.getMinFee();
+					maxFee = arg0.getMaxFee();
+					text = "Game fee (" + minFee + " - " + maxFee
+					+ "): ";
+					showChooseFeeDialog(title, text);
+				} else {// error
+					text = "Game fee (" + minFee + " - " + maxFee
+					+ "): ";
+					showChooseFeeDialog(title, text);
+				}
+			}
+
+			private void showChooseFeeDialog(final String title,
+					final String text) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						menuContext);
+				alert.setTitle(title);
+				alert.setMessage(text);
+				// Set an EditText view to get user input
+				final EditText input = new EditText(menuContext);
+
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int whichButton) {
+
+						String value = input.getText().toString();
+						int fee = 0;
+						try {
+							fee = Integer.parseInt(value);
+						} catch (NumberFormatException e) {
+							showFeeErrorToast();
+							return;
 						}
+
+						if ((fee < minFee) || (fee > maxFee)) {
+							showFeeErrorToast();
+							return;
+						}
+
+						// invokes the CreateNewGame() method of
+						// the TurnBasedGames object.
+						// this method creates a new game that
+						// is represented in the lobby and
+						// opened for other users to join
+						startGameWithChosenFee(fee);
+
+						return;
 					}
 
-					private void showChooseFeeDialog(final String title,
-							final String text) {
-						AlertDialog.Builder alert = new AlertDialog.Builder(
-								menuContext);
-						alert.setTitle(title);
-						alert.setMessage(text);
-						// Set an EditText view to get user input
-						final EditText input = new EditText(menuContext);
-
-						alert.setView(input);
-						alert.setPositiveButton("Ok",
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,int whichButton) {
-
-										String value = input.getText().toString();
-										int fee = 0;
-										try {
-											fee = Integer.parseInt(value);
-										} catch (NumberFormatException e) {
-											showFeeErrorToast();
-											return;
-										}
-
-										if ((fee < minFee) || (fee > maxFee)) {
-											showFeeErrorToast();
-											return;
-										}
-
-										// invokes the CreateNewGame() method of
-										// the TurnBasedGames object.
-										// this method creates a new game that
-										// is represented in the lobby and
-										// opened for other users to join
-										startGameWithChosenFee(fee);
-
-										return;
-									}
-
-									private void showFeeErrorToast() {
-										Toast.makeText(menuContext,"Invalid fee value!",Toast.LENGTH_SHORT).show();
-										showChooseFeeDialog(title, text);
-									}
-								});
-
-						alert.setNegativeButton("Cancel",
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,int which) {
-										return;
-									}
-								});
-
-						alert.show();
+					private void showFeeErrorToast() {
+						Toast.makeText(menuContext,"Invalid fee value!",Toast.LENGTH_SHORT).show();
+						showChooseFeeDialog(title, text);
 					}
-
 				});
+
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int which) {
+						return;
+					}
+				});
+
+				alert.show();
+			}
+
+		});
 	}
 
 	/*
@@ -195,69 +206,69 @@ public class GameController {
 		skMorris.getGameManager().getTurnBasedTools().createNewGame(fee, null, null, new StartGame());
 		GameController.getInstance().clearGame();
 		GameController.getInstance().setWaiting_for_opponent(true);
-		
-		
+
+
 		Intent intent = new Intent(GameController.getInstance().getMenuContext(), PlayGameActivity.class);
 		GameController.getInstance().getMenuContext().startActivity(intent);
 	}
-	
+
 	/*
 	 * sendInformation() method - Standard game move, sending payload with as a String with the x,y coordinates/id's
 	 */
 	public void sendInformation(String payload, int event, String chat){
 		skMorris.getGameManager().getTurnBasedTools().makeGameMove(game_id, event, payload, chat, new GameMove());
 	}	
-	
-	
+
+
 	/*
 	 * handleMyMove() method - handles my move according to recieved x and y data.
 	 * Invotes the suitable communication method when needed
 	 */
 	public void handleMyMove(int x, int y){
-		
+
 	}
-	
+
 	/*
 	 *  handleOpponentMove() method - handles the opponent move according to received game_state.
 	 *  invokes the suitable communication method for every game_state.
 	 */	
 	public void handleOpponentMove(int game_state, String game_id, String Opponentpayload){
 		GameController.getInstance().setGameStarted(true);
-		
+
 		switch(game_state){
 		case SKTurnBasedTools.GAME_STATE_WON:
 			GameController.getInstance().setServerEndGameresponse(true);
 			break;
-			
+
 		case SKTurnBasedTools.GAME_STATE_LOST:
 			GameController.getInstance().setServerEndGameresponse(true);
 			break;
-			
+
 		case SKTurnBasedTools.GAME_STATE_TIED:
 			GameController.getInstance().setServerEndGameresponse(true);
 			break;
-			
+
 		case SKTurnBasedTools.GAME_STATE_ARE_YOU_HERE:
 			skMorris.getGameManager().getTurnBasedTools().makeGameMove(game_id, SKTurnBasedTools.GAME_EVENT_STILL_HERE, "", null, new GameMove());
-		
+
 		default:
-			
+
 			String strx = Opponentpayload.substring(0, 1);
 			String stry = Opponentpayload.substring(1, 2);
-			
+
 			int x = Integer.parseInt(strx);
 			int y = Integer.parseInt(stry);
-			
+
 			GameController.getInstance().makeMove(x, y);
-			
+
 			GameController.getInstance().switchTurns();
 			//NOW CHECK IF SOMEONE IS WINNING, NEEDS MOAR LOGIC
-			
+
 			break;
 		}
 	}
-	
-	
+
+
 	/*
 	 * makeMove() method - gets the coordinates and updates the data structure
 	 * *MANGLER LOGIKK*
@@ -265,8 +276,8 @@ public class GameController {
 	public boolean makeMove(int x, int y){
 		return true;
 	}
-	
-	
+
+
 	/*
 	 *  SwitchTurns() method - switches the turns between the users
 	 */
@@ -278,7 +289,7 @@ public class GameController {
 			this.setTurn(1);
 		}
 	}
-	
+
 	/*
 	 * showToastOnCanvas() method - shows a specified toast message on the canvas
 	 */
@@ -288,18 +299,18 @@ public class GameController {
 			@Override
 			public void run() {
 				Toast.makeText(GameController.getInstance().getCanvasContext(), string, Toast.LENGTH_SHORT).show();
-				
+
 			}
-			
+
 		};
 		((Activity)(GameController.getInstance().getCanvasContext())).runOnUiThread(toastAction );
 	}
-	
-	
+
+
 	public void setTurn(int turn){
 		this.turn = turn;
 	}
-		
+
 	public int getSide(){
 		return side;
 	}
@@ -311,11 +322,11 @@ public class GameController {
 	public boolean isServerEndGameresponse(){
 		return serverEndGameresponse;
 	}
-	
+
 	public void setServerEndGameresponse(boolean serverEndGameresponse){
 		this.serverEndGameresponse = serverEndGameresponse;
 	}
-	
+
 	public boolean isGameStarted() 
 	{
 		return gameStarted;
@@ -325,7 +336,7 @@ public class GameController {
 	{
 		this.gameStarted = gameStarted;
 	}
-	
+
 	public boolean isPrinted(){
 		return printed;
 	}
@@ -335,7 +346,7 @@ public class GameController {
 	public void setPrinted(boolean printed) {
 		this.printed = printed;
 	}
-	
+
 	public String getGame_id() {
 		return game_id;
 	}
@@ -391,7 +402,7 @@ public class GameController {
 	public void setCanvasContext(Context canvasContext) {
 		this.canvasContext = canvasContext;
 	}
-	
+
 	public boolean isCanvasContextON() {
 		return canvasContextON;
 	}
@@ -399,7 +410,7 @@ public class GameController {
 	public void setCanvasContextON(boolean canvasContextON) {
 		this.canvasContextON = canvasContextON;
 	}
-	
+
 	public Context getMenuContext() {
 		return menuContext;
 	}
@@ -448,6 +459,69 @@ public class GameController {
 	 */
 	public static Game getMorrisGame() {
 		return morrisGame;
+	}
+
+	public static void handlePlayerAction(Point p) {
+		if (morrisGame.getState() instanceof PlacementState) {
+			for (int i = 0; i < morrisGame.getPlayer1().getPieces().size(); i++) {
+				Piece piece = morrisGame.getPlayer1().getPieces().get(i);
+				if (piece.getPosition() < 0) {
+					piece.setPosition(p.getId());
+					morrisGame.playerPlacedPiece(morrisGame.getPlayer1(),piece, p.getId());
+					morrisGame.getBoard().reserveModelPoint(p.getId(), piece);  
+					break;
+				}
+			}
+		}
+		else if(morrisGame.getState() instanceof SelectState){
+			if(morrisGame.selectable(morrisGame.getPlayer1(), p.getId())){
+				morrisGame.updatePieceImages(morrisGame.getPlayer1(), p.getId());
+				morrisGame.setState(new MoveState());
+			}
+		}
+		else if(morrisGame.getState() instanceof MoveState){
+			morrisGame.updatePieceImages(morrisGame.getPlayer1(), p.getId());
+			if(morrisGame.getPlayer1().getSelectedPiece().getPosition()==p.getId()){
+				morrisGame.setState(new SelectState());
+			}
+			else{
+				morrisGame.move(morrisGame.getPlayer1().getSelectedPiece(), p.getId(), morrisGame.getPlayer1()); // SISTE PARAMETER ER SPILLER ID
+				if(morrisGame.getState() instanceof RemovalState){
+					morrisGame.updatePieceImages(morrisGame.getPlayer1(), p.getId());
+				}
+				else{
+					morrisGame.setState(new SelectState());
+				}
+
+			}
+
+		}
+		else if(morrisGame.getState() instanceof RemovalState){
+			//getPlayer1() just for testing, must be changed to getPlayer2()
+			morrisGame.removePiece(p, morrisGame.getPlayer1());
+			morrisGame.setState(new SelectState());
+			morrisGame.updatePieceImages(morrisGame.getPlayer1(), p.getId());
+
+		}
+		else if(morrisGame.getState() instanceof FlyingState){
+			if(morrisGame.getPlayer1().getSelectedPiece().getPosition()==p.getId()){
+				morrisGame.setState(new SelectState());
+			}else{
+				morrisGame.move(morrisGame.getPlayer1().getSelectedPiece(), p.getId(), morrisGame.getPlayer1()); // SISTE PARAMETER ER SPILLER ID
+				morrisGame.setState(new SelectState());
+
+			}
+		}
+	}
+	//Player1 is used for testing, must change with getPlayingPlayer()
+	public static ArrayList<ModelPoint> getHighlightsList() {
+		ArrayList<ModelPoint> highlights; 
+		if(morrisGame.getPlayer1().getSelectedPiece() != null){					
+			highlights = morrisGame.getHighlightList(morrisGame.getPlayer1().getSelectedPiece().getPosition(), morrisGame.getPlayer1());  
+		} else {
+			highlights = morrisGame.getHighlightList(-1, morrisGame.getPlayer1());  
+		}
+		return highlights;
 	}
 
 }
