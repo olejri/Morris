4fea708f1de188044ba5a9f04e87d4eb57482bd6
Stian Sorebo <stian.soerebo@gmail.com>
2012-04-19 14:47:51 +0200

    Signed-off-by: Stian Sorebo <stian.soerebo@gmail.com>

--- Morris/src/morris/models/Game.java
+++ Morris/src/morris/models/Game.java
@@ -380,12 +380,14 @@ public class Game implements NetworkListener {
 		this.currentPlayer = currentPlayer;
 	}
 
-	public void changePlayer(){
-		if (currentPlayer == player1){
-			setCurrentPlayer(player2);
-		}
-		else {
-			setCurrentPlayer(player1);
+	public void changePlayer(boolean hotseat){
+		if(hotseat){
+			if (currentPlayer == player1){
+				setCurrentPlayer(player2);
+			}
+			else {
+				setCurrentPlayer(player1);
+			}
 		}
 	}
 
@@ -430,7 +432,7 @@ public class Game implements NetworkListener {
 				break;
 			}
 		}
-		changePlayer();
+		changePlayer(true);
 		
 	}
 
